13ad8fa9883785ccf8aa0013bbf394cb20431efc
Marc Dumais <marc.dumais@ericsson.com>
2024-07-18 08:58:48 -0400

    Test dynamically allocating each thread_data struct
    In this version I went back to each worker using "result" from
    the passed struct but allocated each struct dynamically so they
    would end-up most likely not being next to eachother in memory.
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- sum_of_squares.c
+++ sum_of_squares.c
@@ -15,33 +15,32 @@ typedef struct {
 void* compute_sum_of_squares(void* arg) {
     ThreadData* data = (ThreadData*)arg;
     struct timeval start_time, end_time;
-    long long result = 0;
 
     gettimeofday(&start_time, NULL);
     data->result = 0;
     for (long i = data->start; i <= data->end; ++i) {
-        result += (unsigned long long)i * i;
+        data -> result += (unsigned long long)i * i;
     }
     gettimeofday(&end_time, NULL);
     data->time_taken = (end_time.tv_sec - start_time.tv_sec) +
                        (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
-    data->result = result;
 
     pthread_exit(NULL);
 }
 
-double work(int num_workers, unsigned long long start_num, unsigned long long end_num, pthread_t *threads, ThreadData *thread_data) {
+double work(int num_workers, unsigned long long start_num, unsigned long long end_num, pthread_t *threads, ThreadData **thread_data) {
     struct timeval start_time, end_time;
     unsigned long long chunk_size = (end_num - start_num + 1) / num_workers;
+
     gettimeofday(&start_time, NULL);
-    for (unsigned long long i = 0; i < num_workers; ++i) {
-        thread_data[i].start = start_num + i * chunk_size;
-        thread_data[i].end = thread_data[i].start + chunk_size - 1UL;
+    for (int i = 0; i < num_workers; ++i) {
+        thread_data[i]->start = start_num + i * chunk_size;
+        thread_data[i]->end = thread_data[i]->start + chunk_size - 1UL;
         if (i == num_workers - 1) {
-            thread_data[i].end = end_num; // Ensure last worker handles any remainder
+            thread_data[i]->end = end_num; // Ensure last worker handles any remainder
         }
 
-        pthread_create(&threads[i], NULL, compute_sum_of_squares, &thread_data[i]);
+        pthread_create(&threads[i], NULL, compute_sum_of_squares, thread_data[i]);
     }
 
     for (int i = 0; i < num_workers; ++i) {
@@ -50,29 +49,43 @@ double work(int num_workers, unsigned long long start_num, unsigned long long en
 
     gettimeofday(&end_time, NULL);
     return (end_time.tv_sec - start_time.tv_sec) +
-                        (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+           (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
 }
 
 int main(int argc, char* argv[]) {
     pthread_t threads[MAX_WORKERS];
-    ThreadData thread_data[MAX_WORKERS];
+    ThreadData* thread_data[MAX_WORKERS];
     double total_worker_time = 0;
     double total_time = 0;
     unsigned long long total_work_done = 0;
     unsigned long long start_num = 100L;
     unsigned long long end_num = 5000000000UL;
 
-    for(int i = 1; i <= MAX_WORKERS; i++) {
+    for (int i = 1; i <= MAX_WORKERS; i++) {
+        // Allocate thread_data for each worker dynamically
+        for (int j = 0; j < i; j++) {
+            thread_data[j] = (ThreadData*)malloc(sizeof(ThreadData));
+            if (thread_data[j] == NULL) {
+                perror("Failed to allocate memory for thread_data");
+                exit(EXIT_FAILURE);
+            }
+        }
+
         double iteration_worker_time = 0;
         double total_time_inner = work(i, start_num, end_num, threads, thread_data);
         total_time += total_time_inner;
         for (int j = 0; j < i; j++) {
-            iteration_worker_time += thread_data[j].time_taken;;
-            total_work_done += thread_data[j].result;
-            total_worker_time += thread_data[j].time_taken;
+            iteration_worker_time += thread_data[j]->time_taken;
+            total_work_done += thread_data[j]->result;
+            total_worker_time += thread_data[j]->time_taken;
         }
         printf("Time for %d workers (wall time, total workers time): %f / %f\n",
-            i, total_time_inner, iteration_worker_time);
+               i, total_time_inner, iteration_worker_time);
+
+        // Free thread_data for each worker
+        for (int j = 0; j < i; j++) {
+            free(thread_data[j]);
+        }
     }
 
     printf("Total work done (sum of squares from %llu to %llu): %llu\n", start_num, end_num, total_work_done);
